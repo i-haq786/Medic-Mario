7aa1f4794aaa4755466eaf14c0e6dc1b77484c7c
i-haq786 <i.haqcs@gmail.com>
2022-09-04 15:24:20 +0530

    done!

---
+++ lib/element1.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Myelement1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 35,
+      width: 35,
+      child: Image.asset('images/element1.png'),
+    );
+  }
+}
